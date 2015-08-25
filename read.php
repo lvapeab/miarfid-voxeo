dda72089c577939fd3dea3df2b466cb36d8b776e
lvapeab <lvapeab@gmail.com>
2015-08-25 13:07:09 +0200

    Final version

--- read.php
+++ read.php
@@ -1,56 +1,106 @@
-<?php
-  echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-  if (!isset($_GET['uid'])) {
-    trigger_error("Expected message \"uid\".", E_USER_ERROR);
+<?php 
+  echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+  
+  if(!isset($_GET['mode'])){
+    trigger_error("Se esperaba un modo",E_USER_ERROR);
+  }
+
+  if(!isset($_GET['page'])){
+  trigger_error("Se esperaba una página",E_USER_ERROR);
   }
   
-$page_id = $_GET['uid'];
-$access_token = '727478640669562|y2OmHAKuU7NqW1zmLb5I9eU3Bk4';
+  $mode = $_GET['mode'];
+  $mode= str_replace("'","",$mode );
+
+  $page_id = $_GET['page'];
+  $page_id= str_replace("'","",$page_id );
+
+
+  
+  $access_token = '727478640669562|y2OmHAKuU7NqW1zmLb5I9eU3Bk4';
+
 //Get the JSON
-$json_object = @file_get_contents('https://graph.facebook.com/' . $page_id . 
-'/posts?access_token=' . $access_token);
+  $json_object = @file_get_contents('https://graph.facebook.com/' . $page_id . 
+  '/posts?access_token=' . $access_token);
+
 //Interpret data
-$fbdata = json_decode($json_object);
+  $fbdata = json_decode($json_object);
 
 
-foreach ($fbdata->data as $post )
-{
-$posts .= '<p><a href="' . $post->link . '">' . $post->story . '</a></p>';
-$posts .= '<p><a href="' . $post->link . '">' . $post->message . '</a></p>';
-$posts .= '<p>' . $post->description . '</p>';
-$posts .= '<br />';
+  if($mode === "readLast"){
+    $posts .=  $fbdata->data[0]->message ;
 }
-  
-  
-echo $posts;
-?>
-<vxml version="2.1" xmlns="http://www.w3.org/2001/vxml" xml:lang="ca-es">
-  <meta name="maintainer" content="lvapeab@fiv.upv.es" />
+  if($mode === "readTwo"){
+    $posts .=  $fbdata->data[0]->message . '  <break/>  ' ;
+    $posts .=  $fbdata->data[1]->message  ;
+  }
+
+  if($mode === "readAll"){
+    foreach ($fbdata->data as $post )
+    {
+      $posts .=  $post->message . '  <break/>  ' ;
+    }
+  } 
+// Filtramos las webs
+$reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
+// Comprobamos si existe una url en la noticia.
+if(preg_match($reg_exUrl,$posts)) {
+      //Si existe, la eliminamos
+       $posts = preg_replace($reg_exUrl, " ", $posts);
+} 
+
+if(empty($posts)){
+  $posts = "Lo siento, no hay noticias en esa página. <break\>";
+  }
+?>  
+
+
+
+<vxml version="2.1" xmlns="http://www.w3.org/2001/vxml" xml:lang="es-es">
   <form id="MainForm">
-   <block>
+    <block>
       <prompt>
-	<?php echo $posts; ?>
+        <?php
+        echo $posts; 
+        echo "<break/>";
+        echo "¿Deseas hacer algo más?";
+        ?>
       </prompt>
     </block>
+
     <field name="action">
       <grammar xmlns="http://www.w3.org/2001/06/grammar"
-               xml:lang="es-es" root="ROOT" mode="voice">
-        <rule id="ROOT" scope="public">
-          <one-of>
-            <item><ruleref uri="./grammar/control.xml#ROOT"/></item>
-            <item><ruleref uri="./grammar/main.xml#ROOT"/></item>
-            <item><ruleref uri="./grammar/readPosts.xml#ROOT"/></item>
-          </one-of>
-        </rule>
-      </grammar>
-      <filled>
-        <if cond="lastresult$.interpretation.ACTION == 'stop'">
-          <clear namelist="action"/>
-        </if>
-        <if cond="lastresult$.interpretation.ACTION == 'exit'">
-          <prompt>Adiós</prompt>
-        </if>
-      </filled>
-    </field>
-  </form>
+      xml:lang="es-es" root="ROOT" mode="voice">
+      <rule id="ROOT" scope="public">
+        <one-of>
+        <item><ruleref uri="./control.grxml#ROOT"/></item>
+        <item><ruleref uri="./main.grxml#ROOT"/></item>
+      </one-of>
+    </rule>
+  </grammar>
+
+
+  <filled>
+   <if cond="lastresult$.interpretation.ACTION == 'stop'">
+      <clear namelist="action"/>
+        <prompt> . ¿Deseas hacer algo más?</prompt>
+    </if>
+   <if cond="lastresult$.interpretation.ACTION == 'exit'">
+      <prompt>Adiós.</prompt>
+    </if>
+    <if cond="lastresult$.interpretation.ACTION == 'read'">
+     <assign name="page" expr="action$.interpretation.PAGE" />
+     <assign name="mode" expr="action$.interpretation.MODE" />
+     <submit next="read.php" method="get" namelist="mode page" />
+   </if>
+
+    <if cond="lastresult$.interpretation.ACTION == 'repeat'">
+      <submit next="read.php" method="get" namelist="mode page" />
+
+
+    </if>
+  </filled>
+</field>
+
+</form>
 </vxml>
